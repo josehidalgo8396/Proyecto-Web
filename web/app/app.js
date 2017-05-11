b706e5530e0afe201227311febd8a22131b0ae4c
josehidalgo8396 <josehidalgo8396@gmail.com>
2017-05-11 11:29:18 -0600

    Login Facebook

--- web/app/app.js
+++ web/app/app.js
@@ -34,6 +34,21 @@
           parent: 'base',
           templateUrl: 'common/views/promo.html'
         })
-    })
-})();
+    });
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId      : '292548087852966',
+        xfbml      : true,
+        version    : 'v2.9'
+      });
+      FB.AppEvents.logPageView();
+    };
 
+    (function(d, s, id){
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {return;}
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+})();
--- web/app/login/controllers/login.js
+++ web/app/login/controllers/login.js
@@ -39,5 +39,18 @@
 					}
 				});
 			};
+
+			$scope.fbLogin = function() {
+				FB.login(function(response) {
+					if (response.authResponse) {
+						FB.api('/me', {fields: 'name, email'}, function(response) {
+							console.log(response);
+						});
+					} 
+					else {
+						console.log('User cancelled login or did not fully authorize.');
+					}
+				});
+			};
 		}]);
 })();
